049dc9fb102dfb72ae10f0e89bc0db1d6f0d5a0c
Olasupo <olasupoodebiyi@yahoo.com>
2020-08-22 23:01:37 +0100

    Setup basic structure for backend

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,14 +1,15 @@
+import path from "path";
 import { Post } from "./entities/Post";
 import { __prod__ } from "./constants";
 import { MikroORM } from "@mikro-orm/core";
-import path from "path";
+import { User } from "./entities/User";
 
 export default {
   migrations: {
     path: path.join(__dirname, "./migrations"),
     pattern: /^[\w-]+\d+\.[jt]s$/,
   },
-  entities: [Post],
+  entities: [Post, User],
   dbName: "spreddit",
   user: "postgres",
   password: "0000",
